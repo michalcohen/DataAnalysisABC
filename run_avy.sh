5862d58af251a7e20d5c20ee188bc666f7cea83d
michalcohen <cohenmelany1@gmail.com>
2021-05-23 00:29:42 +0300

    no calc

--- run_avy.sh
+++ run_avy.sh
@@ -2,19 +2,12 @@ check_dir(){
 	for file in $1*.aig
 	do
 	echo "${file}"
-	~/extavy/build/avy/src/avypdr ${file} -v 2 > ${file%.aig}.txt
-	~/extavy/build/avy/src/avypdr ${file} -v 2 -q > ${file%.aig}_q.txt
+	#~/extavy/build/avy/src/avypdr ${file} -v 2 > ${file%.aig}.txt
+	#~/extavy/build/avy/src/avypdr ${file} -v 2 -q > ${file%.aig}_q.txt
 	done
 }
 
 for dir in ~/beem/*/
 do
 	check_dir ${dir} &
-	#echo "${dir}"
-    #dir=${dir%*/}      # remove the trailing "/"
-    #echo "${dir##*/}"    # print everything after the final "/"
 done
-# for FILE in *
-# do
-# 	echo -e "$FILE\nLoops Rule\!" > $FILE
-# done
