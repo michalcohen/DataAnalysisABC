8c2171fb7e5ec3f2bc053fb471d9993d3f1add8d
michalcohen <cohenmelany1@gmail.com>
2021-06-05 18:51:02 +0300

    update run_avy

--- run_avy.sh
+++ run_avy.sh
@@ -2,12 +2,12 @@ check_dir(){
 	for file in $1*.aig
 	do
 	echo "${file}"
-	~/michal/extavy/build/avy/src/avypdr ${file} -v 2 > ${file%.aig}.txt
-	~/michal/extavy/build/avy/src/avypdr ${file} -v 2 -q > ${file%.aig}_q.txt
+	~/extavy/build/avy/src/avypdr ${file} -v 2 > ${file%.aig}.txt
+	~/extavy/build/avy/src/avypdr ${file} -v 2 -q > ${file%.aig}_q.txt
 	done
 }
 
-for dir in ~/michal/beem/*/
+for dir in ~/beem/*/
 do
 	check_dir ${dir} &
 done
