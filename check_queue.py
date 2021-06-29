7c7a063472b106c4296f9e52fa4de88df520df38
Assaf Mauda <assaf127@gmail.com>
2021-06-29 09:05:35 +0300

    run with brunch

--- check_queue.py
+++ check_queue.py
@@ -1,31 +1,34 @@
 import os
-import tqdm
-from multiprocess import Pool
 
 max_pool = 12
 files = list()
 
 
-def process_aig(f):
-    print(f)
-    out = f[:f.rindex(".")]
-    os.system(f"~/abc/abc -c \"read {f}; zero; fold; write_aiger {out}_fixed.aig\"")
-    os.system(f"python ~/michal/extavy/avy/ext/brunch.py --out {out}.txt {out}_fixed.aig -v ")
-    os.system(f"~/extavy/build/avy/src/avypdr {out}_fixed.aig -v 2 -q > {out}_q.txt")
-    
+def process_dir(d, out):
+    print(d)
+    for entry in os.scandir(d):
+        base_path = entry.path[:entry.path.rindex(".")]
+        if entry.is_file() and entry.path.endswith(".aig") and (not entry.path.endswith("_fixed.aig") and not os.path.isfile(base_path + "_fixed.aig")):
+            os.system(f"/home/assaf/michal/abc/abc -c \"read {entry.path}; zero; fold; write_aiger {base_path}_fixed.aig\"")
 
-def scan_directory(dir_name):
+    # os.system(f"python /home/assaf/michal/extavy/avy/ext/brunch.py --out {out}.txt {out}_fixed.aig -v 2")
+    # os.system(f"python /home/assaf/michal/extavy/avy/ext/brunch.py --out {out}_q.txt {out}_fixed.aig -v 2 -q")
+
+    os.system(f"/home/assaf/michal/extavy/avy/ext/brunch.py --no-ts --out {out} --format base:Cpu:Frames:Invar:run:Result {d}/*_fixed.aig -- /home/assaf/michal/extavy/build/avy/src/avypdr -v 2 {{f}}")
+    os.system(f"/home/assaf/michal/extavy/avy/ext/brunch.py --no-ts --out {out}_q --format base:Cpu:Frames:Invar:run:Result {d}/*_fixed.aig -- /home/assaf/michal/extavy/build/avy/src/avypdr -v 2 -q {{f}}")
+
+
+def scan_subdirectories(dir_name, output_file):
     for entry in os.scandir(dir_name):
-        if entry.is_file() and entry.path.endswith(".aig") and not entry.path.endswith("_fixed.aig"):
-            files.append(entry.path)
-        elif entry.is_dir():
-            scan_directory(entry.path)
+        if entry.is_dir():
+            process_dir(entry.path, output_file)
+            scan_subdirectories(entry.path, output_file)
 
 
 def main(root):
-    scan_directory(root)
-    with Pool(max_pool) as p:
-        pool_outputs = list(tqdm.tqdm(p.imap(process_aig, files), total=len(files)))
+    scan_subdirectories(root, "results")
+    # with Pool(max_pool) as p:
+    #     pool_outputs = list(tqdm.tqdm(p.imap(process_dir, files), total=len(files)))
 
 
 if __name__ == "__main__":
